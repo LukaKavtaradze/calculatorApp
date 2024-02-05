73d513ec45607db6f29f2404788fec3fda8e2da9
Luka Kavtaradze <Lk@gmail.com>
2024-02-05 18:22:30 +0400

    calculator

--- script.js
+++ script.js
@@ -2,15 +2,84 @@ let previousValue = document.getElementById("prev-value");
 let currentValue = document.getElementById("current-Value");
 let calcKeyboard = document.getElementById("calc-keyboard");
 const reset = document.getElementById("reset");
+
+
 const deleteValue = document.getElementById("del");
 
 
+
+const dashboard = document.getElementById("dashboard");
+
+const calculate = document.getElementById("calculate");
 
 let sqema = ` `;
 let operatorButton;
 let Num;
 
+const body = document.querySelector("body");
+const modes = document.getElementById("bg-mode");
+const board = document.getElementById("board");
+const numbers = document.querySelectorAll(".number");
 
+const themes = document.querySelectorAll(".theme-col");
+
+modes.addEventListener("input", () => {
+  themes.forEach((item) => {
+    numbers.forEach((num) => {
+      if (modes.value === "1") {
+        body.style.background = "#3a4663";
+        item.style.color = "white";
+        previousValue.style.color = "white";
+        currentValue.style.color = "white";
+        dashboard.style.background = "#181f33";
+        board.style.background = "#242d44";
+        deleteValue.style.background = "#647198";
+        deleteValue.style.boxShadow = " inset 0 -4px 0 0 #414e73";
+        num.style.background = "#eae3dc";
+        num.style.color = "#434a59";
+        num.style.boxShadow = "  inset 0 -4px 0 0 #b3a497";
+        reset.style.background = "#647198";
+        reset.style.boxShadow = " inset 0 -4px 0 0 #414e73";
+        calculate.style.background = "#d03f2f";
+        calculate.style.boxShadow = " inset 0 -4px 0 0 #93261a";
+      }
+      if (modes.value === "2") {
+        body.style.background = "#e6e6e6";
+        item.style.color = "#36362c";
+        previousValue.style.color = "#36362c";
+        currentValue.style.color = "#36362c";
+        dashboard.style.background = "#eeeeee";
+        board.style.background = "#d2cdcd";
+        num.style.background = "#e5e4e1";
+        num.style.color = "#36362c";
+        num.style.boxShadow = "  inset 0 -4px 0 0 #a79e91";
+        deleteValue.style.background = "#378187";
+        deleteValue.style.boxShadow = "inset 0 -4px 0 0 #1b6066";
+        reset.style.background = "#378187";
+        reset.style.boxShadow = " inset 0 -4px 0 0 #1b6066";
+        calculate.style.background = "#c85402";
+        calculate.style.boxShadow = " inset 0 -4px 0 0 #873901";
+      }
+      if (modes.value === "3") {
+        body.style.background = "#17062a";
+        item.style.color = "#ffe53d";
+        previousValue.style.color = "#ffe53d";
+        currentValue.style.color = "#ffe53d";
+        dashboard.style.background = "#1e0936";
+        board.style.background = "#1e0936";
+        num.style.background = "#331c4d";
+        num.style.color = "#ffe53d";
+        num.style.boxShadow = " inset 0 -4px 0 0 #be15f4";
+        deleteValue.style.background = "#56077c";
+        deleteValue.style.boxShadow = "inset 0 -4px 0 0 #be15f4";
+        reset.style.background = "#56077c";
+        reset.style.boxShadow = " inset 0 -4px 0 0 #be15f4";
+        calculate.style.background = "#00ded0";
+        calculate.style.boxShadow = " inset 0 -4px 0 0  #6cf9f1";
+      }
+    });
+  });
+});
 
 reset.addEventListener("click", () => {
   currentValue.textContent = "0";
@@ -18,15 +87,13 @@ reset.addEventListener("click", () => {
 
 function deleteNum() {
   Num = currentValue.textContent;
- let deletedNum = Num.slice(0, -1)
+  let deletedNum = Num.slice(0, -1);
   currentValue.textContent = deletedNum;
-  if(currentValue.textContent === "") {
-    currentValue.textContent ="0"
+  if (currentValue.textContent === "") {
+    currentValue.textContent = "0";
   }
   console.log(deletedNum);
 }
-
-
 
 function calculation(calculate) {
   let summary = eval(calculate).toFixed(2);
@@ -34,7 +101,6 @@ function calculation(calculate) {
   previousValue.textContent = "0";
   console.log(summary);
 }
-
 
 calcKeyboard.addEventListener("click", (e) => {
   let keyButton = e.target;
@@ -74,6 +140,9 @@ calcKeyboard.addEventListener("click", (e) => {
     calculation(sqema);
   }
   if (clickedKey === "DEL") {
-    deleteNum()
+    deleteNum();
   }
 });
+
+
+
